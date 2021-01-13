832c7906b35c4b2d0b782d45395f5df318bd89d8
Volkan Temel <68102722+VolkanTemel@users.noreply.github.com>
2021-01-13 18:45:51 +0300

    Enter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import List from './List/List'
+import List from './List'
 
 export default function App() {
   return (
--- src/Components/List.js
+++ src/Components/List.js
@@ -4,8 +4,20 @@ import './Lists.css';
 
 export default class List extends Component {
     state = {
-        todos: [ "work hard", "test test test test", "sssssssssssssssssssssssssssssssssssssssssssssss" ]
-    }
+        todos: [ "work hard", "test test test test", "sssssssssssssssssssssssssssssssssssssssssssssss" ],
+        todo:""
+
+    };
+
+    todoss = (e) => {
+        this.setState({todo:e.target.value});
+    };
+
+    addTodo = () => {
+        let a = [...this.state.todos];
+        a.push(this.state.todo);
+        this.setState({todos:a});
+    };
 
     render() {
         return (
@@ -15,9 +27,10 @@ export default class List extends Component {
                         <li key={ index }>{ todo }</li>
                     )) }
                 </ul>
+                <p>{this.state.todo}</p>
                 <div className="bottom-div">
-                    <input className="input" placeholder="Enter Some To-Do"></input>
-                    <button className="button">Add</button>
+                    <input className="input" placeholder="Enter Some To-Do" onChange={this.todoss}></input>
+                    <button className="button" onClick={this.addTodo}>Add</button>
                 </div>
             </div >
         )
